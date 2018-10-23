bb826b7b791a3b7362c1d9763d7cc5148a895411
TestAndunR <andunranmal@gmail.com>
2018-10-23 09:58:41 +0530

    commit to a different repo

---
+++ addNewProject/lambda.js
@@ -0,0 +1,8 @@
+let AWS = require('aws-sdk');
+console.log("asasas")
+exports.handler = function(event, context, callback) {
+    
+
+    console.log("User test for vcs state in git commit");
+    callback(null,'Successfully executed');
+}
